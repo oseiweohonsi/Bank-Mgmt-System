4eecc4c3881d3bf9b0989724a3a6d9e46769dd96
oseiweohonsi <oseiweohonsi@gmail.com>
2023-08-14 16:25:28 -0400

    created a main menu

--- main.cpp
+++ main.cpp
@@ -5,10 +5,19 @@
  * and withdrawing from a simulated bank account
 ***********************************************************/
 #include <iostream>
+#include <fstream> // file operations header file
 void displayMenu(); // shows the menu of the bank application
 int main()
 {
+    displayMenu();
     return 0;
 }
 
-
+void displayMenu()
+{
+    std::cout << "**********Bank Management System**********" << std::endl;
+    std::cout << "1. Create Account\n";
+    std::cout << "2. Deposit\n";
+    std::cout << "3. Withdraw\n";
+    std::cout << "4. Quit\n";
+}

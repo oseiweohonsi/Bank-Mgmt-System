b8df6a2656497404068265f800357e51a3e1a3dd
oseiweohonsi <oseiweohonsi@gmail.com>
2023-08-14 18:49:11 -0400

    Added needed private variables

--- Account.h
+++ Account.h
@@ -1,22 +1,19 @@
 #ifndef ACCOUNT_H
 #define ACCOUNT_H
+#include <string>
 class Account
 {
 private:
-    double savingsDeposit; // deposit made into a savings acccount
-    double checkingsDeposit;    // deposit made into a checkings account
-    double accBalance;  // Balance of the account
+    double savingsDeposit;   // deposit made into a savings acccount
+    double checkingsDeposit; // deposit made into a checkings account
+    double accBalance;       // Balance of the account
+    std::string userName;    // account holder's name
+    std::string firstName;
+    std::string middleName;
+    std::string lastName;
+    char *DOB; // holds the account holder's date of birth
 public:
-    Account(/* args */);
-    ~Account();
+    void getUserName(); // gets the account holder's name
 };
-
-Account::Account(/* args */)
-{
-}
-
-Account::~Account()
-{
-}
 
 #endif

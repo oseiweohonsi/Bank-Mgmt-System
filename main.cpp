e23a6b00d5b59df7ae13d3c1c6d18a4bd94dd92c
oseiweohonsi <oseiweohonsi@gmail.com>
2023-08-14 12:59:50 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/***********************************************************
+ * Author: Oseiwe Zion
+ * Description: Bank Management system project for creating, 
+ * depositing
+ * and withdrawing from a simulated bank account
+***********************************************************/
+#include <iostream>
+void displayMenu(); // shows the menu of the bank application
+int main()
+{
+    return 0;
+}
+
+
